191e7075eca2fb7323dd5a2b8360dc2b9e683b52
DaWasteh <crazyapokalypse@googlemail.com>
2026-05-19 22:21:29 +0200

    v2.0 fixed ruff

--- tuner.pyi
+++ tuner.pyi
@@ -12,7 +12,7 @@ from __future__ import annotations
 
 import re
 from dataclasses import dataclass, field
-from typing import Any, Dict, List, Optional, Tuple
+from typing import Any, Dict, List, Optional
 
 from hardware import SystemInfo
 from performance_target import PerformanceTarget
